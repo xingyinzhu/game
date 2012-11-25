3f31711b4d01468ac96e2f33bf4e633d249093cd
XingyinZhu <xingyinzhu@gmail.com>
2012-11-25 11:14:46 +0800

    Initial Commit

---
+++ Game/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Game
+//
+//  Created by Xingyin Zhu on 12-11-25.
+//  Copyright (c) 2012年 Xingyin Zhu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Game/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Game
+//
+//  Created by Xingyin Zhu on 12-11-25.
+//  Copyright (c) 2012年 Xingyin Zhu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
