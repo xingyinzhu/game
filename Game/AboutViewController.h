d530a969284352805ccd546f46e18d4d5e0ad224
XingyinZhu <xingyinzhu@gmail.com>
2012-11-30 22:17:51 +0800

    first commit

---
+++ Game/AboutViewController.h
@@ -0,0 +1,13 @@
+//
+//  AboutViewController.h
+//  Game
+//
+//  Created by Xingyin Zhu on 12-11-26.
+//  Copyright (c) 2012年 Xingyin Zhu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AboutViewController : UIViewController
+
+@end
